e42ec05a3640734312ca02542e9d68836af5f35d
Sergio Da Silva <sergiodasilva.dev@gmail.com>
2023-12-12 10:22:03 -0400

    added pages with their respective folders

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,9 +1,20 @@
+import Home from '../Home'
+import MyAccount from '../MyAccount'
+import MyOrder from '../MyOrder'
+import MyOrders from '../MyOrders'
+import NotFound from '../NotFound'
+import SignIn from '../SignIn'
 import './App.css'
 
 function App() {
   return (
     <div className='bg-red-100'>
-      Hola mundo bebé
+      <Home />
+      <MyAccount />
+      <MyOrder />
+      <MyOrders />
+      <NotFound />
+      <SignIn />
     </div>
   )
 }
