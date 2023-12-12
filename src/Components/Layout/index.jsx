0e2ab1e734526fceb525c036393a43c82c66e72b
Sergio Da Silva <sergiodasilva.dev@gmail.com>
2023-12-12 17:02:24 -0400

    Created card component and connection to api

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,9 +1,4 @@
-import PropTypes from 'prop-types'
-
 const Layout = ({ children }) => {
-  Layout.propTypes = {
-    children: PropTypes.node.isRequired,
-  }
   return (
     <div className='flex flex-col items-center mt-20'>
       {children}
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react"
+import Card from "../../Components/Card"
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products')
+        const data = await response.json()
+        setItems(data)
+      } catch (error) {
+        console.error(`Oh no, ocurrió un error: ${error}`);
+      }
+    }
+    fetchData()
+  }, [])
   return (
     <div>
       Home
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {
+          items?.map(item => (<Card key={item.id} data={item} />))
+        }
+      </div>
     </div>
   )
 }
