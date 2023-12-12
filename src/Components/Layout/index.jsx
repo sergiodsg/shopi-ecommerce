b1734bda5f3adaf6f11e3175b5f990eb895aa5cf
Sergio Da Silva <sergiodasilva.dev@gmail.com>
2023-12-12 11:54:32 -0400

    Installed and added proptypes to eliminate eslint error in Layout component

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,4 +1,9 @@
+import PropTypes from 'prop-types'
+
 const Layout = ({ children }) => {
+  Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+  }
   return (
     <div className='flex flex-col items-center mt-20'>
       {children}
