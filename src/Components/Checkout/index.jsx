9079d2ff8bd9e3d8d046485ffa2f28b52a31e848
Sergio Da Silva <sergiodasilva.dev@gmail.com>
2023-12-19 11:24:44 -0400

    Added scroll to checkout

--- src/Components/Checkout/index.jsx
+++ src/Components/Checkout/index.jsx
@@ -14,7 +14,7 @@ const Checkout = () => {
 
   return (
     <>
-    <div className="w-1/2 p-3 bg-slate-50 rounded-lg">
+      <div className="w-1/2 p-5 bg-slate-50 rounded-lg">
         <div className="flex justify-between w-full">
           <h2 className="font-medium text-xl">My Order</h2>
           <div
@@ -24,26 +24,40 @@ const Checkout = () => {
             <XMarkIcon className="h-6 w-6 text-black"></XMarkIcon>
           </div>
         </div>
-    {
-      context.cartProducts.map((product, index) => (
-        <div className="flex justify-start w-full h-14 mt-4" key={product.id}>
-          <img className="w-10 h-10 rounded-lg" src={product.images[0]} alt={product.title} />
-          <div className="flex justify-between w-full ml-4">
-            <div>
-            <p className="font-medium text-md mr-3">{product.title}</p>
-            <p className="font-semibold text-md">${product.price}</p>
+        <div className="max-h-96 my-3 mr-1 overflow-y-scroll">
+        {context.cartProducts.map((product, index) => (
+          <div className="flex justify-start w-full h-14 mt-4" key={product.id}>
+            <img
+              className="w-10 h-10 rounded-lg"
+              src={product.images[0]}
+              alt={product.title}
+            />
+            <div className="flex justify-between w-full ml-4">
+              <div>
+                <p className="font-medium text-md mr-3">{product.title}</p>
+                <p className="font-semibold text-md">${product.price}</p>
+              </div>
+              <XCircleIcon
+                className="h-6 w-6 text-black cursor-pointer mr-4"
+                onClick={() => removeProduct(index)}
+              ></XCircleIcon>
             </div>
-            <XCircleIcon className="h-6 w-6 text-black cursor-pointer" onClick={() => removeProduct(index)}></XCircleIcon>
           </div>
-        </div>))
-    }
-    <div className="flex justify-between w-full mt-4">
-      <p className="font-semibold text-lg">Total:</p>
-      <p className="font-semibold text-lg">${context.cartProducts.reduce((acc, product) => acc + product.price, 0)}</p>
-    </div>
-    </div>
+        ))}
+        </div>
+        <div className="flex justify-between w-full mt-4">
+          <p className="font-semibold text-lg">Total:</p>
+          <p className="font-semibold text-lg">
+            $
+            {context.cartProducts.reduce(
+              (acc, product) => acc + product.price,
+              0
+            )}
+          </p>
+        </div>
+      </div>
     </>
   );
-}
+};
 
 export default Checkout;
